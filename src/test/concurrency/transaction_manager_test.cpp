fa7d2671786547b23e9999c6aec493cecefa699e
Fabian Engel <engelfab@gmail.com>
2019-02-14 12:14:33 +0100

    Format and lint TA test.

--- src/test/concurrency/transaction_manager_test.cpp
+++ src/test/concurrency/transaction_manager_test.cpp
@@ -1,15 +1,12 @@
-
-
 #include "base_test.hpp"
 #include "gtest/gtest.h"
 
-#include "concurrency/transaction_manager.hpp"
 #include "concurrency/transaction_context.hpp"
+#include "concurrency/transaction_manager.hpp"
 
 namespace opossum {
 
 class TransactionManagerTest : public BaseTest {
-
  protected:
   void SetUp() override {}
 
@@ -26,7 +23,6 @@ class TransactionManagerTest : public BaseTest {
 // destructor of the transaction context but is called
 // manually for this test.
 TEST_F(TransactionManagerTest, TrackActiveCommitIDs) {
-
   EXPECT_EQ(get_active_snapshot_commit_ids().size(), 0);
 
   const auto t1_context = manager().new_transaction_context();
@@ -34,26 +30,32 @@ TEST_F(TransactionManagerTest, TrackActiveCommitIDs) {
   const auto t3_context = manager().new_transaction_context();
 
   EXPECT_EQ(get_active_snapshot_commit_ids().size(), 3);
-  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(), t1_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
-  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(), t2_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
-  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(), t3_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
+  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
+                        t1_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
+  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
+                        t2_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
+  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
+                        t3_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
   EXPECT_EQ(manager().get_lowest_active_snapshot_commit_id(), t1_context->snapshot_commit_id());
 
   t1_context->commit();
   manager().deregister_transaction(t1_context->snapshot_commit_id());
-  
+
   EXPECT_EQ(get_active_snapshot_commit_ids().size(), 2);
-  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(), t1_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
-  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(), t3_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
+  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
+                        t1_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
+  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
+                        t3_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
   EXPECT_EQ(manager().get_lowest_active_snapshot_commit_id(), t2_context->snapshot_commit_id());
 
   t3_context->commit();
   manager().deregister_transaction(t3_context->snapshot_commit_id());
 
   EXPECT_EQ(get_active_snapshot_commit_ids().size(), 1);
-  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(), t2_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
+  EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
+                        t2_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
   EXPECT_EQ(manager().get_lowest_active_snapshot_commit_id(), t2_context->snapshot_commit_id());
-    
+
   t2_context->commit();
   manager().deregister_transaction(t2_context->snapshot_commit_id());
 
