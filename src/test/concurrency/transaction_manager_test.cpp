38baad40c7046bcba559b79755a0aa225e359c92
Julian Menzler <julianmenzler@gmail.com>
2019-02-15 16:14:51 +0100

    Fix and adjust TransactionManager test & add it to CMakeLists.txt

--- src/test/concurrency/transaction_manager_test.cpp
+++ src/test/concurrency/transaction_manager_test.cpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <vector>
+
 #include "base_test.hpp"
 #include "gtest/gtest.h"
 
@@ -10,57 +13,76 @@ class TransactionManagerTest : public BaseTest {
  protected:
   void SetUp() override {}
 
-  TransactionManager& manager() { return TransactionManager::get(); }
-
   static std::unordered_multiset<CommitID>& get_active_snapshot_commit_ids() {
     return TransactionManager::get()._active_snapshot_commit_ids;
   }
+
+  static void register_transaction(CommitID snapshot_commit_id) {
+    TransactionManager::get().register_transaction(snapshot_commit_id);
+  }
+  static void deregister_transaction(CommitID snapshot_commit_id) {
+    TransactionManager::get().deregister_transaction(snapshot_commit_id);
+  }
 };
 
-// Check if all active snapshot commit ids of uncommitted
-// transaction contexts are tracked correctly.
-// Normally, deregister_transaction() is called in the
-// destructor of the transaction context but is called
-// manually for this test.
+/** Check if all active snapshot commit ids of uncommitted
+ * transaction contexts are tracked correctly.
+ * Normally, deregister_transaction() is called in the
+ * destructor of the transaction context but is called
+ * manually for this test.
+ */
 TEST_F(TransactionManagerTest, TrackActiveCommitIDs) {
-  EXPECT_EQ(get_active_snapshot_commit_ids().size(), 0);
+  auto& manager = TransactionManager::get();
 
-  const auto t1_context = manager().new_transaction_context();
-  const auto t2_context = manager().new_transaction_context();
-  const auto t3_context = manager().new_transaction_context();
+  EXPECT_EQ(get_active_snapshot_commit_ids().size(), 0);
+  EXPECT_EQ(manager.get_lowest_active_snapshot_commit_id(), MvccData::MAX_COMMIT_ID);
+
+  const auto t1_context = manager.new_transaction_context();
+  const auto t2_context = manager.new_transaction_context();
+  const auto t3_context = manager.new_transaction_context();
+
+  const CommitID t1_snapshot_commit_id = t1_context->snapshot_commit_id();
+  const CommitID t2_snapshot_commit_id = t2_context->snapshot_commit_id();
+  const CommitID t3_snapshot_commit_id = t3_context->snapshot_commit_id();
+  const auto vec = std::vector<CommitID>{t1_snapshot_commit_id, t2_snapshot_commit_id, t3_snapshot_commit_id};
 
   EXPECT_EQ(get_active_snapshot_commit_ids().size(), 3);
   EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
-                        t1_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
+                        t1_snapshot_commit_id) != get_active_snapshot_commit_ids().cend());
   EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
-                        t2_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
+                        t2_snapshot_commit_id) != get_active_snapshot_commit_ids().cend());
   EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
-                        t3_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
-  EXPECT_EQ(manager().get_lowest_active_snapshot_commit_id(), t1_context->snapshot_commit_id());
+                        t3_snapshot_commit_id) != get_active_snapshot_commit_ids().cend());
+  EXPECT_EQ(manager.get_lowest_active_snapshot_commit_id(), *std::min_element(vec.cbegin(), vec.cend()));
 
   t1_context->commit();
-  manager().deregister_transaction(t1_context->snapshot_commit_id());
+  deregister_transaction(t1_context->snapshot_commit_id());
 
   EXPECT_EQ(get_active_snapshot_commit_ids().size(), 2);
   EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
                         t1_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
   EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
                         t3_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
-  EXPECT_EQ(manager().get_lowest_active_snapshot_commit_id(), t2_context->snapshot_commit_id());
+  EXPECT_EQ(manager.get_lowest_active_snapshot_commit_id(), t2_context->snapshot_commit_id());
 
   t3_context->commit();
-  manager().deregister_transaction(t3_context->snapshot_commit_id());
+  deregister_transaction(t3_context->snapshot_commit_id());
 
   EXPECT_EQ(get_active_snapshot_commit_ids().size(), 1);
   EXPECT_TRUE(std::find(get_active_snapshot_commit_ids().cbegin(), get_active_snapshot_commit_ids().cend(),
                         t2_context->snapshot_commit_id()) != get_active_snapshot_commit_ids().cend());
-  EXPECT_EQ(manager().get_lowest_active_snapshot_commit_id(), t2_context->snapshot_commit_id());
+  EXPECT_EQ(manager.get_lowest_active_snapshot_commit_id(), t2_context->snapshot_commit_id());
 
   t2_context->commit();
-  manager().deregister_transaction(t2_context->snapshot_commit_id());
+  deregister_transaction(t2_context->snapshot_commit_id());
 
   EXPECT_EQ(get_active_snapshot_commit_ids().size(), 0);
-  EXPECT_EQ(manager().get_lowest_active_snapshot_commit_id(), MvccData::MAX_COMMIT_ID);
+  EXPECT_EQ(manager.get_lowest_active_snapshot_commit_id(), MvccData::MAX_COMMIT_ID);
+
+  // To prevent exceptions in TransactionContext destructor
+  register_transaction(t1_snapshot_commit_id);
+  register_transaction(t2_snapshot_commit_id);
+  register_transaction(t3_snapshot_commit_id);
 }
 
 }  // namespace opossum
